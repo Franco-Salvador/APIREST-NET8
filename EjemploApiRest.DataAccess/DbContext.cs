abf2bc67477d001a7d2442ed6eab5526eb91f9b0
Franco Antonio Salvador Torres <fsalvadort1@upao.edu.pe>
2025-01-17 18:30:38 -0500

    Agregar referencia Abstractions
    Agregar referencia Abstractions

--- EjemploApiRest.DataAccess/DbContext.cs
+++ EjemploApiRest.DataAccess/DbContext.cs
@@ -1,7 +1,27 @@
-﻿namespace EjemploApiRest.DataAccess
+﻿using System.Data;
+using EjemploApiRest.Abstractions;
+namespace EjemploApiRest.DataAccess
 {
-    public class Dbcontext<T> 
+    public class DbContext<T> : IDbContext<T>
     {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
 
+        public IList<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Save(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
